86150b355237482fdeea0346d0c246336ef0b71a
swetachovatiya23 <swetachovatiya2312@gmail.com>
2022-03-07 16:28:39 -0500

    added cursor pointer

--- finesse/src/components/Navbar.jsx
+++ finesse/src/components/Navbar.jsx
@@ -1,15 +1,51 @@
 import React from 'react'
 import styled from 'styled-components'
 
-
+//React styled component in use
 const Container = styled.div `
   height: 60px;
   background-color: pink;
 `
+
+//Parent-wrapper for nav 
+const Wrapper = styled.div `
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+flex:1;`
+const Center = styled.div`
+flex:1;`
+const Right = styled.div`
+flex:1;`
+
+
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+  `
+
 const Navbar = () => {
   return (
     <Container>
-      Navbar
+      <Wrapper>
+          <Left>
+            <Language>EN</Language>
+            {/* <SearchContainer>
+              input
+              icon
+            </SearchContainer> */}
+          </Left>
+
+          <Center>Center</Center>
+
+          <Right>Right</Right>
+          
+          
+      </Wrapper>
+      
     </Container>
   )
 }
