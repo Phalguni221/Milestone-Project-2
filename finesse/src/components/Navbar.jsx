3b294bf07e63692d375214ed8f41ff9ab5268d32
swetachovatiya23 <swetachovatiya2312@gmail.com>
2022-03-07 17:48:04 -0500

    NavBar added

--- finesse/src/components/Navbar.jsx
+++ finesse/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
-import { Search } from '@material-ui/icons';
+import { Badge } from "@material-ui/core";
+import { Search,ShoppingCartOutlined } from '@material-ui/icons';
 import React from 'react'
 import styled from 'styled-components'
 
@@ -46,12 +47,30 @@ const Left = styled.div`
   `;
 
 const Center = styled.div`
-  flex: 1;`;
+  flex: 1;
+  text-align: center;
+  `;
 
+//logo/name
+const Logo = styled.h1`
+  font-weight: bold;
+`;
+
+//For Right side things
 const Right = styled.div`
-  flex: 1;`
-  ;
+  flex: 1;
+  display: flex;
+  aline-item: center;
+  justify-content: flex-end;
+  `;
 
+
+ //for menuitems, cart and all
+ const MenuItem = styled.div`
+ font-size: 14px;
+ cursor: pointer;
+ margin-left: 25px;
+ `; 
 
 const Navbar = () => {
   return (
@@ -62,13 +81,31 @@ const Navbar = () => {
 
             <SearchContainer>
               <Input/>
-              <Search/>
+              <Search style={{ color: "purple", fontSize: 16 }}/>
             </SearchContainer> 
           </Left>
 
-          <Center>Center</Center>
+          <Center>
+            <Logo>
+              FINESSE
+            </Logo>
+            
+          </Center>
 
-          <Right>Right</Right>
+          <Right>
+            <MenuItem>
+              REGISTER
+            </MenuItem>
+            <MenuItem>
+              SIGN IN
+            </MenuItem>
+            <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlined />
+            </Badge>
+            </MenuItem>
+
+          </Right>
           
           
       </Wrapper>
