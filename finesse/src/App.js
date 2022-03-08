7fae83f5461b91baa6b6ea259a7420164d68b028
Sriya Bhamidipati <88012398+Phalguni221@users.noreply.github.com>
2022-03-08 11:50:09 -0500

    Update App.js

--- finesse/src/App.js
+++ finesse/src/App.js
@@ -1,38 +1,9 @@
-import { BrowserRouter, Link } from 'react-router-dom'
+import Home from "./pages/Home";
 
-import './App.css';
-
-import Navbar from './components/Navbar'
-import Home from './components/Home'
-import Footer from './components/Footer'
-
-
-
-function App() {
-
-  return (
-    <div className="App">
-     <BrowserRouter>
-        <header>
-          <h1 className="title">Welcome to Finesse</h1>
-
-          <div className="navBar">
-              <Navbar />
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/Sign-In">Sign-in</Link>
-              </li>
-            </ul>
-          </div>
-        </header>
-     
-        <footer>
-            <Footer />
-        </footer>
-        </BrowserRouter>
+const App = () => {
+    return <Home/>;
+};
+export default App;
   </div>
   );
 }
