73c63c1f9729f6fad4857fd0bc6374d0d5271682
Sriya Bhamidipati <88012398+Phalguni221@users.noreply.github.com>
2022-03-08 11:51:17 -0500

    Update Home.js

--- finesse/src/components/Home.js
+++ finesse/src/components/Home.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import Navbar from './Navbar';
-import Footer from './Footer';
-import './App.css'
+import Navbar from '../components/Navbar';
 
 
-export default function Home () {
+
+const Home = () => {
     return (
         <div>
-            <head>
-      
-            </head>
-        <body>
-        <p>Welcome to Finesse!
-            We are committed to providing you with the latest and most comfort 
-        </p>
-        </body>
-        <footer>
-         
-        </footer>
+            <Navbar/>
         </div>
     )
 }
+
+export default Home;
 
