7f795d5543e7371d084197e1a8ebbccca1b0e52f
Venkatesh Bhamidipati <vbhamidipati@yahoo.com>
2022-03-09 11:03:01 -0500

    Changed name

--- finesse/src/components/Register.js
+++ finesse/src/components/Register.js
@@ -23,7 +23,7 @@ export default function Register() {
     <div>
       <Container2>
         <Wrapper2>
-        <h1>New Member Sign-up Form</h1>
+        <h1>New Member Registration Form</h1>
     <h2>If you want to become a new member of Finesse, please fill out and submit the questionnaire below!</h2>
     <br></br>
     <br></br>
